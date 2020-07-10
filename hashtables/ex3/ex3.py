d848bf56043dc2ce966cc40a75aac29217a4e87c
Anthony K <antonyk@users.noreply.github.com>
2020-07-10 11:42:41 -0400

    intersections of multiple lists (ex3)

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,28 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    base_cache = {}
+
+    # get shortest
+    shortest = []
+    if len(arrays) > 0:
+        shortest = arrays[0]
+    for _, v in enumerate(arrays):
+        shortest = v if len(v) < len(shortest) else shortest
+
+    # populate base cache, based on the shortest number list
+    for num in shortest:
+        base_cache[num] = None
+
+    for _, v in enumerate(arrays):
+        if v != shortest:
+            new_cache = {}
+            for num in v:
+                if num in base_cache:
+                    new_cache[num] = None
+            
+            base_cache = new_cache
+            
+    result = list(base_cache.keys())
 
     return result
 
